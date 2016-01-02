7b9eb583463e8972aeeded0b144e313bd8d1d377
Jeevan <jeevan.murkoth@inte.com>
2016-01-02 14:32:22 -0600

    fixed namespace

--- src/TodoMVCRC1/TagHelper/ToDoTagHelper.cs
+++ src/TodoMVCRC1/TagHelper/ToDoTagHelper.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TodoMVCRC1.Models;
+using ToDo.Core.Models;
 
 namespace TodoMVCRC1.TagHelpers
 {
