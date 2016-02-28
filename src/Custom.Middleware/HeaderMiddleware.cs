e876bccfca0e730352732096c7fcd66a39dbd129
Jeevan <jeevan.murkoth@inte.com>
2016-02-28 07:27:33 -0600

    Added new Extension method and used options

--- src/Custom.Middleware/HeaderMiddleware.cs
+++ src/Custom.Middleware/HeaderMiddleware.cs
@@ -15,16 +15,20 @@ namespace Custom.Middleware
     {
         private readonly RequestDelegate _next;
         private ILogger<HeaderMiddleware> _logger;
-        
-        public HeaderMiddleware(RequestDelegate next, ILogger<HeaderMiddleware> logger)
+        private HeaderOptions _options;
+        public HeaderMiddleware(RequestDelegate next, ILogger<HeaderMiddleware> logger, HeaderOptions options )
         {
             _next = next;
             _logger = logger;
+            _options = options;
         }
         public async Task Invoke(HttpContext context)
         {
             _logger.LogInformation("***********************Invoked custom MiddleWare***********************");
-            context.Response.Headers.Add("Powered By", "ASPNET CORE");
+            if (!string.IsNullOrEmpty( _options?.HeaderName))
+            {
+                context.Response.Headers.Add(_options.HeaderName, _options.HeaderValue);
+            }
             await _next.Invoke(context);
             _logger.LogInformation("*******************Final Invoked custom MiddleWare *********************");
         }
---
+++ src/Custom.Middleware/HeaderOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Custom.Middleware
+{
+    public class HeaderOptions
+    {
+        public string HeaderName { get; set; }
+        public string HeaderValue { get; set; }
+    }
+}
